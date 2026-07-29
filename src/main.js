9383844b2fc83e09b2acad95d8111b09126cf5ff
Yassin Soliman <yassin.soliman.120@gmail.com>
2026-07-29 04:06:51 +0300

    some of teh code

--- src/main.js
+++ src/main.js
@@ -1,30 +1,80 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer(); 
-renderer.setSize(window.innerWidth , window.innerHeight); 
+import earthTextur from "./assets/earth.png";
+import sunTexture from "./assets/8k_sun.png";
+import jupiterTexture from "./assets/jupiter";
+import marsTexture from "./assets/mars.png";
+import mercuryTexture from "./assets/mercury.png";
+import moonTexture from "./assets/moon.png";
+import neptuneTexture from "./assets/neptune.png";
+import saturnTexture from "./src/assets/saturn.png";
+import uranusTexture from "./assets/uranus.png";
+import venusTexture from "./assets/venus.png";
+import { textureLoad } from 'three/src/nodes/TSL.js';
+
+const renderer = new THREE.WebGLRender(); 
+renderer.setsize(window.innerWidth,innerHeight);
+renderer.setPixelRatio(Window.devicepixel);
 document.body.appendChild(renderer.domElement);
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    75 , 
-    window.innerWidth / window.innerHeight , 
+const scence = new THREE.Scene(); 
+const camera = new THREE.camera(
+    45 , 
+    Window.innerWidth / window.innerHeight, 
     0.1 , 
-    1000 
-); 
-const axiss = new THREE.AxesHelper(3); 
-scene.add(axiss); 
-camera.position.set(0,2,5);
+    1000
+);
+const orbit = new OrbitControls (camera , renderer.domElement); 
+camera.position.set(-90 , 140 ,140); 
+orbit.update(); 
 
-const boxgeo = new THREE.BoxGeometry(); 
-const bomaterial = new THREE.MeshBasicMaterial({color : 0x00FF00});
-const box = new THREE.Mesh(boxgeo,bomaterial);
-function animation () { 
-    box.rotation.x += 0.1; 
-    box.rotation.y += 0.01 ;
-    box.rotation.z += 0.01 ;
-    renderer.render(scene,camera);
+const ambientLight = new THREE.AmbientLight('#555555');
+scence.add(ambientLight);
+
+const textureloader = new THREE.textureloader();
+
+renderer.setcolor('#111111');
+
+const sungeo = new THREE.SphereGeometry(16 , 30 , 30); 
+const sunmat = new THREE.MeshBasicMaterial({
+    map : textureloader.load(sunTexture),
+    emmissive : 0xfffff, 
+    emmissivesensivity : 1
+
+}); 
+
+const sun = new THREE.Mesh(sungeo , sunmat); 
+scence.add(sun); 
+
+const pointlight = new THREE.pointlight('#ffffff' , 2 , 300); 
+scence.add(pointlight); 
+
+const createplant = (size , texture , position ,ring) => { 
+    const geo = new THREE.SphereGeometry(size , 30 , 30); 
+    const mat = new THREE.MeshStandardMaterial({
+        map : textureLoad.load(texture) 
+    });
+
+const mesh = new THREE.Mesh(geo, mat)
+const obj = new THREE.Object3D()
+obj.add(mesh)
+if(ring){
+    const ringGeo = new THREE.RingGeometry(ring.innerRadius, ring.outerRadius, 32)
+    const ringMat = new THREE.MeshBasicMaterial({
+        map: textureLoader.load(ring.texture),
+        transparent: true,
+        opacity: 0.9,
+        side: THREE.DoubleSide
+    })
+    const ringMesh = new THREE.Mesh(ringGeo, ringMat)
+    obj.add(ringMesh)
+    ringMesh.position.x = position
+    ringMesh.rotation.x = -0.5 * Math.PI
+}    
+};
+
+function animate () {
+    renderer.render(scence,camera);    
 }
-scene.add(box);
-renderer.setAnimationLoop(animation);
-
+renderer.setanimateloop(animate);
